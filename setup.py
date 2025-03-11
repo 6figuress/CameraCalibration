510421c52c1827ee75268ead7e65c65e6c7368cf
Dimitri Imfeld <33431953+imfeldd@users.noreply.github.com>
2025-03-11 13:43:17 +0100

    Update setup.py
    :-)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=["camera_sync", "opencv-python", "matplotlib", "open3d", "scipy", "bpy", "numpy<2"],
-    python_requires=">=3.11,<3.12",
+    python_requires=">=3.10,<3.12",
     author="6 Figures",
     description="A package to allow camera to localize aruco markers in a 3d world",
 )
