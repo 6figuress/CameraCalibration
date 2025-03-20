ef88036d7db1c1c5661bd086b0f66734ab3a9b70
MartTave <martin.tavernier256@gmail.com>
2025-03-20 15:04:00 +0100

    Fixes

--- setup.py
+++ setup.py
@@ -4,15 +4,24 @@ setup(
     name="camera_sync",
     version="0.1",
     packages=find_packages(),
-    install_requires=["camera_sync", "opencv-python", "matplotlib", "open3d", "scipy", "bpy", "numpy"],
+    install_requires=[
+        "camera_sync",
+        "opencv-python",
+        "opencv-contrib-python",
+        "matplotlib",
+        "open3d",
+        "scipy",
+        "bpy",
+        "numpy",
+    ],
     python_requires=">=3.10,<3.12",
     author="6 Figures",
     description="A package to allow camera to localize aruco markers in a 3d world",
     package_data={
-        "camera_sync": [ 
+        "camera_sync": [
             "calibration/*",
             "blender/base.blend",
             "blender/10_to_15.png",
         ],
-    }
+    },
 )
